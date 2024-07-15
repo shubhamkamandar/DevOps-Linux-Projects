10c552278c2dbefae0f30d1403a87667bd26b910
Shubham Kamandar <93696457+shubhamkamandar@users.noreply.github.com>
2024-07-15 10:57:23 +0530

    Create group_mgmt.sh

---
+++ group_mgmt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+create_group() {
+    read -p "Enter group name: " groupname
+    sudo addgroup "$groupname"
+}
+
+delete_group() {
+    read -p "Enter group name to delete: " groupname
+    sudo delgroup "$groupname"
+}
+
+add_user_to_group() {
+    read -p "Enter username: " username
+    read -p "Enter group name: " groupname
+    sudo adduser "$username" "$groupname"
+}
+
+# Main script
+echo "Group Management Script"
+echo "1. Create Group"
+echo "2. Delete Group"
+echo "3. Add User to Group"
+read -p "Enter your choice: " choice
+
+case $choice in
+    1) create_group ;;
+    2) delete_group ;;
+    3) add_user_to_group ;;
+    *) echo "Invalid choice";;
+esac
